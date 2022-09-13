771933471d4499a99d300b6a5c21b864e6873b56
Nadia Bacchus <nadiabacchus@Nadias-MacBook-Pro.local>
2022-09-13 14:24:02 -0400

    updated path for markdown

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
-const generateMarkdown = require("utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 //Create an array of questions for user input
 const questions = [
